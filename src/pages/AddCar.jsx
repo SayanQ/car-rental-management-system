31ab207435b33eca563773e1cab68e32cb1de9d8
Sayan Sadhukhan <sadhukhansayan1234@gmail.com>
2023-06-23 11:47:52 +0530

    Third

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form } from 'semantic-ui-react';
 import axios from "../assets/axios.jsx";
-import "../styles/customer-details.css";
+import "../styles/addCar.css";
 
 const AddCar = () => {
     const [carInput, setCarInput] = useState({
@@ -40,50 +40,72 @@ const AddCar = () => {
   console.log(carInput);
 
     return (
-        <div>
+        <div className="outer">
+        <div className="box">
             <Form className="create-form">
+                <div className="element-div">
                 <Form.Field>
                     <label>car_No</label>
                     <input placeholder='car_No' className = 'car_No' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>model</label>
                     <input placeholder='model' className = 'model' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>company</label>
                     <input placeholder='company' className = 'company' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>type</label>
                     <input placeholder='type' className = 'type' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>colour</label>
                     <input placeholder='colour' className = 'colour' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>year_Of_Manufacturing</label>
                     <input type = "number" placeholder='year_Of_Manufacturing' className = 'year_Of_Manufacturing' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>km_Driven</label>
                     <input type = "number" placeholder='km_Driven' className = 'km_Driven' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>sitting_Capacity</label>
                     <input type = "number" placeholder='sitting_Capacity' className = 'sitting_Capacity' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>boot_space</label>
                     <input type = "number" placeholder='boot_space' className = 'boot_space' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
+                </div>
+                <div className="element-div">
                 <Form.Field>
                     <label>charges_Per_Hour</label>
                     <input type = "number" placeholder='charges_Per_Hour' className = 'charges_Per_Hour' onChange={(e) => handleInputChange(e)}/>
                 </Form.Field>
-                <Button onClick={(e) => postData(e)} type='submit'>Submit</Button>
+                </div>
+                <Button className='sub-btn' onClick={(e) => postData(e)} type='submit'>Submit</Button>
             </Form>
+        </div>
         </div>
     ); 
 }
